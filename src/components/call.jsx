6594957690ecf77f81f920951f915947b69af592
ayush-ctzn <ayush.sinha@clevertap.com>
2022-05-31 12:39:35 +0530

    hardcoded pankaj's cuid for teeting

--- src/components/call.jsx
+++ src/components/call.jsx
@@ -28,8 +28,8 @@ export const Call = ({ name, email }) => {
 
   const makeCall = () => {
     const cuids = ['ayush.sinha@clevertap.com', 'sumantu@clevertap.com', 'shivam.sharma@celvertap.com', 'darshan.pania@clevertap.com', 'sumantudc@clevertap.com']
-    const callee = cuids[Math.floor(Math.random() * cuids.length)];
-    dcClient.call(callee, 'Introductroy Call').then(res => toast('call ' + res)).catch(err => {
+    // const callee = cuids[Math.floor(Math.random() * cuids.length)];
+    dcClient.call('pankajdc@clevertap.com', 'Introductroy Call').then(res => toast('call ' + res)).catch(err => {
       if (err && err.message) {
         toast(err.message)
       } else {
