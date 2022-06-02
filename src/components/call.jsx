403423484cec3d64f9f66a7a17f087bc6e715c30
ayush-ctzn <ayush.sinha@clevertap.com>
2022-06-02 15:46:49 +0530

    ̉added list of callees to be called

--- src/components/call.jsx
+++ src/components/call.jsx
@@ -27,9 +27,9 @@ export const Call = ({ name, email }) => {
   }, [email, name, clevertap])
 
   const makeCall = () => {
-    // const cuids = ['ayush.sinha@clevertap.com', 'sumantu@clevertap.com', 'shivam.sharma@celvertap.com', 'darshan.pania@clevertap.com', 'sumantudc@clevertap.com']
-    // const callee = cuids[Math.floor(Math.random() * cuids.length)];
-    dcClient.call('pankajdc@clevertap.com', 'Introductroy Call').then(res => toast('call ' + res)).catch(err => {
+    const cuids = ['ayush.sinha@clevertap.com', 'sumantu@clevertap.com', 'shivam.sharma@celvertap.com', 'darshan.pania@clevertap.com', 'sumantudc@clevertap.com']
+    const callee = cuids[Math.floor(Math.random() * cuids.length)];
+    dcClient.call(callee, 'Introductroy Call').then(res => toast('call ' + res)).catch(err => {
       if (err && err.message) {
         toast(err.message)
       } else {
