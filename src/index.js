be7db681d798906d71e31222c719f72ee7c494df
ayush-ctzn <ayush.sinha@clevertap.com>
2022-07-22 16:25:15 +0530

    fixed validation and ct sdk

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 let Clevertap = clevertap
 Clevertap.privacy.push({ optOut: false });
 // Clevertap.init('W9R-486-4W5Z');
-Clevertap.init('48K-W44-556Z', 'sk1', 'wzrkt.com')
+Clevertap.init('48K-W44-556Z', 'sk1')
 Clevertap.setLogLevel(3);
 window.clevertap = Clevertap
 ReactDOM.render(
